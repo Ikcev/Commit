49638aff5b238802a6e1271821a665c19ae066e2
Alejandro Gomez <Alejandro Gomez@HZ114823.H012108.NET>
2022-12-05 13:44:37 +0100

    Primer Commit Gestor de Usuarios

---
+++ src/sources/Main.java
@@ -0,0 +1,20 @@
+
+package sources;
+
+import java.io.FileNotFoundException;
+
+/**
+ * @author Eñaut Agirre
+ *
+ */
+public class Main {
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		GestorUsuariosApp gua = new GestorUsuariosApp();
+		gua.run();
+	}
+
+}
